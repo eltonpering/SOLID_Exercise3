985a8e35ff8065ecaa8422cf2feeeee73818e902
Elton A Pering <elton_pering@adsomos.com>
2020-07-28 22:10:33 -0300

    Add project files.

---
+++ CursoFoop_Exercicio3/ICalcularImpostoPais.cs
@@ -0,0 +1,9 @@
+﻿namespace CursoFoop_Exercicio3
+{
+    interface ICalcularImpostoPais
+    {
+        decimal TotalRenda{ get; set; }
+        decimal TotalDeducao { get; set; }
+        decimal CalcularValorImposto();
+    }
+}
